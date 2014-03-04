c1dc5c087d5c7af0473fa7f41d86dcac2931cdc4
Adbeel Itamar <adbeel@boloflix.com>
2014-03-03 17:25:00 -0700

    Allow only one picture by user

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,8 +51,8 @@ class User
   embeds_one :contact, as: :contactable,  autobuild: true
   has_many :properties, dependent: :destroy
   belongs_to :company_account, class_name: 'Account', inverse_of: :agents
-  has_many :activities
-  has_many :picture, :dependent => :destroy
+  has_many :activities, :dependent => :destroy
+  has_one :picture, :dependent => :destroy
 
   validates_presence_of :email, :first_name, :last_name, :active, :password, :role
   validates_length_of :password, :within => 6..128
