22d1dedaff2556cffff5e1082d71c139bf2938a1
Amin Ogarrio <amin.ogarrio@gmail.com>
2014-10-23 12:44:55 -0700

    Configure email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Secondhouz::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Print deprecation notices to the Rails logger.
@@ -28,4 +28,14 @@ Secondhouz::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                 587,
+    domain:               'houz.io',
+    user_name:            'adbeel@houz.io',
+    password:             'AkYMLezl9Ac6LMu6-yldSg',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
